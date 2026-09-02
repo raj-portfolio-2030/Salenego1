29b132fdb6a8a0369cbce511310fd8f069e8758b
Raj-Portfolio <raj.siva2030@gmail.com>
2026-09-02 18:26:02 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/Salenego1/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
